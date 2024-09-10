c23ea4f1ab8bb0db91ff1d62e17d3bfc6c071601
kee08 <keerthy8101@gmail.com>
2024-09-10 15:35:57 +0530

    updated file

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+
+const message = process.env.MESSAGE || 'Hello World';
 
 app.get('/', (req, res) => {
-  res.send('Hello from Dockerized Node.js App!');
+  res.send(message);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
--- dockerfile
+++ dockerfile
@@ -3,5 +3,6 @@ WORKDIR /usr/src/app
 COPY package*.json ./
 RUN npm install
 COPY . .
+ENV MESSAGE="Hello World From Docker"
 EXPOSE 3000
 CMD ["node", "app.js"]
